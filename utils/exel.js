6c5c77bfcd9420267901d607e51e3bdcb66cf6c7
Andrey <57412446+podmost777@users.noreply.github.com>
2022-12-07 21:38:31 +0200

    fix

--- utils/exel.js
+++ utils/exel.js
@@ -8,29 +8,30 @@ const saveProducts = (products, title) => {
 
 	const files = fs.readdirSync(path.join(__dirname, "..", "/tmp"));
 
+	console.log("I'm hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
 	console.log(files);
 
-	fs.writeFileSync(path.join(__dirname, "..", "/tmp", newFilename), "");
+	// fs.writeFileSync(path.join(__dirname, "..", "/tmp", newFilename), "");
 
-	const file = reader.readFile(path.join(__dirname, "..", "/tmp", newFilename));
+	// const file = reader.readFile(path.join(__dirname, "..", "/tmp", newFilename));
 
-	ws["!cols"] = [
-		{ wch: 17 },
-		{ wch: 100 },
-		{ wch: 40 },
-		{ wch: 40 },
-		{ wch: 15 },
-		{ wch: 70 },
-		{ wch: 22 },
-	];
+	// ws["!cols"] = [
+	// 	{ wch: 17 },
+	// 	{ wch: 100 },
+	// 	{ wch: 40 },
+	// 	{ wch: 40 },
+	// 	{ wch: 15 },
+	// 	{ wch: 70 },
+	// 	{ wch: 22 },
+	// ];
 
-	products.forEach((product, index) => {
-		ws["F" + (index + 2)].l = { Target: product["Ссылка на товар"] };
-	});
+	// products.forEach((product, index) => {
+	// 	ws["F" + (index + 2)].l = { Target: product["Ссылка на товар"] };
+	// });
 
-	reader.utils.book_append_sheet(file, ws, "Sheet2");
+	// reader.utils.book_append_sheet(file, ws, "Sheet2");
 
-	reader.writeFile(file, path.join(__dirname, "..", "/tmp", newFilename));
+	// reader.writeFile(file, path.join(__dirname, "..", "/tmp", newFilename));
 
 	return newFilename;
 };
